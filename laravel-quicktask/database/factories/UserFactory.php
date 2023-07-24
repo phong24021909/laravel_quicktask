1369bd57c1f219cb36ed408d7f6253c632c8df98
phongthieu <72301235+phong24021909@users.noreply.github.com>
2023-07-24 16:16:17 +0700

    chapter5

--- laravel-quicktask/database/factories/UserFactory.php
+++ laravel-quicktask/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -15,11 +16,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),// password
             'remember_token' => Str::random(10),
+            'is_admin' => false,
+            'is_active' => True,
+            'username' => $this->faker->name(),
         ];
     }
 
--- laravel-quicktask/database/seeders/DatabaseSeeder.php
+++ laravel-quicktask/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Task;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
+        Task::factory(10)->create();
     }
 }
